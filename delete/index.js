f0148158aee15c434d44c7634e3a0d863f3bc1c4
Simon Panek <paneks19@gmail.com>
2020-12-30 19:56:37 -0800

    all lambdas and routes working

--- delete/index.js
+++ delete/index.js
@@ -1,2 +1,20 @@
 'use strict';
 const peopleModel = require('./people.schema.js');
+
+exports.handler = async (event) => {
+  const id = event.pathParameters.id
+
+  try {
+    await peopleModel.delete(id);
+    let data = {};
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      response: JSON.stringify(e.message)
+    }
+  }
+};
